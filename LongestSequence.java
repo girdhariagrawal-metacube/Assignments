3b85601cdab27bb9fb02d7973a1b00a4e734ddae
Neel <neel@meta.com>
2017-07-14 13:55:21 +0530

    these are 5 assignments

--- LongestSequence.java
+++ LongestSequence.java
@@ -1,10 +1,10 @@
 public class LongestSequence {
 
 	public int[] longestSequence(int input[]){
-		int size = input.length, count = 1, max=0, index = 0, k=0;
+		int size = input.length, count = 1, i, max = 0, index = 0, k=0;
 
-		for (int i=0; i < size - 1; i++){
-			if (input[i] < input[i+1]){
+		for (i=0; i < size - 1; i++){
+			if ((input[i] < input[i+1])){
 				count++;
 			}
 			else{
@@ -15,16 +15,24 @@ public class LongestSequence {
 				}
 			}
 		}
+			
+		if (count > max){
+			max = count;
+			count = 1;
+			index = i;
+		}
+				
 		int seq[] = new int[max];
 		for (int j = (index - max + 1); j <= index; j++){
 			seq[k] = input[j];
 			k++;
 		}
-		for (int j = (index - max + 1); j <= index; j++){
+		/*for (int j = (index - max + 1); j <= index; j++){
 			System.out.println(input[j]);
 		}
 		System.out.println(max);
 		System.out.println(index);
+		*/
 		return seq;
 	}
 }
